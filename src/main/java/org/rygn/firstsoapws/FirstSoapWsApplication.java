b324e45285cd245e564c2a8073fcb80097c51801
rgirodon <remy.girodon@gmail.com>
2020-10-25 09:55:42 +0100

    Initial setup and TU

---
+++ src/main/java/org/rygn/firstsoapws/FirstSoapWsApplication.java
@@ -0,0 +1,13 @@
+package org.rygn.firstsoapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSoapWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rygn/firstsoapws/FirstSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rygn.firstsoapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
